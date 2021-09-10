ae60268b59c96a0091d685d98d329289306eb1ab
kirby123-cmd <kirubaselvi40805@gmail.com>
2021-09-10 10:34:48 +0530

    deleted the while loop to keep it simple

--- tictac.py
+++ tictac.py
@@ -115,11 +115,3 @@ def main():
     if isBoardFull(board):
         print('Tie Game!')
 
-while True:
-    answer = input('Do you want to play again? (Y/N)')
-    if answer.lower() == 'y' or answer.lower == 'yes':
-        board = [' ' for x in range(10)]
-        print('-----------------------------------')
-        main()
-    else:
-        break
